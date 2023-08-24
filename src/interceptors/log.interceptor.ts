f65bf3cd85231d502510c47ab3d0a42778da315e
Everton Mota <mcostagt@gmail.com>
2023-08-24 18:38:13 -0300

    module user

---
+++ src/interceptors/log.interceptor.ts
@@ -0,0 +1,15 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { Observable, tap } from "rxjs";
+
+export class LogInterceptor implements NestInterceptor{
+    
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        
+        const dt = Date.now();
+
+        return next.handle().pipe(tap( () => {
+            console.log(`Execucao levou : ${Date.now() - dt } milisendos.`);
+        }));
+    }
+    
+}
